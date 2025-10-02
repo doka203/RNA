4306941bce30904c91f234e747699f0aeda07422
“doka203” <pedrosilva220113@gmail.com>
2025-10-02 09:36:46 -0300

    feat: divisão da base de dados em base de treino e teste

--- RNA/LeitorBase.java
+++ RNA/LeitorBase.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class LeitorBase {
@@ -24,13 +26,13 @@ public class LeitorBase {
 
                     double[] saidas = new double[1];
                     // Converte a classe 2 para 0 e se for 1, mantém 1
-                    if (Double.parseDouble(valores[3]) == 2){
+                    if (Double.parseDouble(valores[3]) == 2) {
                         saidas[0] = 0.0;
-                    }else{
+                    } else {
                         saidas[0] = 1.0;
                     }
-                    
-                    baseList.add(new double[][]{entradas, saidas});
+
+                    baseList.add(new double[][] { entradas, saidas });
                 }
             }
         } catch (IOException e) {
@@ -39,5 +41,92 @@ public class LeitorBase {
         }
 
         return baseList.toArray(new double[0][][]);
+    }
+
+    public static double[][][] lerBaseOrdenada(String caminhoArquivo, boolean ordenar) {
+        double[][][] base = lerBase(caminhoArquivo);
+
+        if (base != null && ordenar) {
+            // Ordena a base de dados pela coluna de saída (crescente)
+            Arrays.sort(base, (amostra1, amostra2) -> Double.compare(amostra1[1][0], amostra2[1][0]));
+        }
+
+        return base;
+    }
+
+    public static class BaseSeparada {
+        public double[][][] baseTreino;
+        public double[][][] baseTeste;
+
+        public BaseSeparada(double[][][] baseTreino, double[][][] baseTeste) {
+            this.baseTreino = baseTreino;
+            this.baseTeste = baseTeste;
+        }
+    }
+
+    public static BaseSeparada separarBase(String caminhoArquivo) {
+        double[][][] baseCompleta = lerBase(caminhoArquivo);
+        return separarBase(baseCompleta);
+    }
+
+    public static BaseSeparada separarBase(double[][][] baseCompleta) {
+        if (baseCompleta == null) {
+            return null;
+        }
+
+        // Separar amostras por classe
+        List<double[][]> classe0 = new ArrayList<>(); // Classe 0 (não-pele)
+        List<double[][]> classe1 = new ArrayList<>(); // Classe 1 (pele)
+
+        for (int i = 0; i < baseCompleta.length; i++) {
+            double[][] amostra = baseCompleta[i];
+            if (amostra[1][0] == 0.0) {
+                classe0.add(amostra);
+            } else {
+                classe1.add(amostra);
+            }
+        }
+
+        // Embaralhar as listas de cada classe para seleção aleatória
+        Collections.shuffle(classe0);
+        Collections.shuffle(classe1);
+
+        // Calcular 75% de cada classe para treino
+        int tamanhoTreinoClasse0 = (int) Math.round(classe0.size() * 0.75);
+        int tamanhoTreinoClasse1 = (int) Math.round(classe1.size() * 0.75);
+
+        // Criar listas para treino e teste
+        List<double[][]> treinoList = new ArrayList<>();
+        List<double[][]> testeList = new ArrayList<>();
+
+        // Adicionar 75% de cada classe (aleatoriamente selecionados) ao treino
+        for (int i = 0; i < tamanhoTreinoClasse0; i++) {
+            treinoList.add(classe0.get(i));
+        }
+        for (int i = 0; i < tamanhoTreinoClasse1; i++) {
+            treinoList.add(classe1.get(i));
+        }
+
+        // Adicionar os 25% restantes ao teste
+        for (int i = tamanhoTreinoClasse0; i < classe0.size(); i++) {
+            testeList.add(classe0.get(i));
+        }
+        for (int i = tamanhoTreinoClasse1; i < classe1.size(); i++) {
+            testeList.add(classe1.get(i));
+        }
+
+        // Converter para arrays
+        double[][][] baseTreino = treinoList.toArray(new double[0][][]);
+        double[][][] baseTeste = testeList.toArray(new double[0][][]);
+
+        /*
+         * Base separada aleatoriamente: 245057 amostras total
+         * - Classe 0 (não-pele): 194198 total (145649 treino, 48549 teste)
+         * - Classe 1 (pele): 50859 total (38144 treino, 12715 teste)
+         * Base de treino: 183793 amostras (75,0%) - selecionadas aleatoriamente
+         * Base de teste: 61264 amostras (25,0%) - selecionadas aleatoriamente
+         */
+
+        return new BaseSeparada(baseTreino, baseTeste);
     }
 }
--- RNA/RnaMain.java
+++ RNA/RnaMain.java
@@ -44,68 +44,73 @@ public class RnaMain {
                 switch (entrada) {
                     case 1:
                         double[][][] baseE = {
-                                { { 0, 0 }, { 0 } }, { { 0, 1 }, { 0 } },
-                                { { 1, 0 }, { 0 } }, { { 1, 1 }, { 1 } }
-                        };
+                                { { 0, 0 }, { 0 } },
+                                { { 0, 1 }, { 0 } },
+                                { { 1, 0 }, { 0 } },
+                                { { 1, 1 }, { 1 } } };
                         if (tipoRede == 1) {
                             redeNeural = new Perceptron(2, 1);
                         } else {
                             redeNeural = new Mlp(2, 2, 1);
                         }
                         treinar(redeNeural, baseE, epocas);
-                        // treinar(redeNeural, baseE);
                         break;
                     case 2:
-                        double[][][] baseOU = {
-                                { { 0, 0 }, { 0 } }, { { 0, 1 }, { 1 } },
-                                { { 1, 0 }, { 1 } }, { { 1, 1 }, { 1 } }
-                        };
+                        double[][][] baseOR = {
+                                { { 0, 0 }, { 0 } },
+                                { { 0, 1 }, { 1 } },
+                                { { 1, 0 }, { 1 } },
+                                { { 1, 1 }, { 1 } } };
                         if (tipoRede == 1) {
                             redeNeural = new Perceptron(2, 1);
                         } else {
                             redeNeural = new Mlp(2, 2, 1);
                         }
-                        treinar(redeNeural, baseOU, epocas);
-                        // treinar(redeNeural, baseOU);
+                        treinar(redeNeural, baseOR, epocas);
                         break;
                     case 3:
                         double[][][] baseXOR = {
-                                { { 0, 0 }, { 0 } }, { { 0, 1 }, { 1 } },
-                                { { 1, 0 }, { 1 } }, { { 1, 1 }, { 0 } }
-                        };
+                                { { 0, 0 }, { 0 } },
+                                { { 0, 1 }, { 1 } },
+                                { { 1, 0 }, { 1 } },
+                                { { 1, 1 }, { 0 } } };
                         if (tipoRede == 1) {
                             redeNeural = new Perceptron(2, 1);
                         } else {
                             redeNeural = new Mlp(2, 2, 1);
                         }
                         treinar(redeNeural, baseXOR, epocas);
-                        // treinar(redeNeural, baseXOR);
                         break;
                     case 4:
                         double[][][] baseRobo = {
-                                { { 0, 0, 0 }, { 1, 1 } }, { { 0, 0, 1 }, { 0, 1 } },
-                                { { 0, 1, 0 }, { 1, 0 } }, { { 0, 1, 1 }, { 0, 1 } },
-                                { { 1, 0, 0 }, { 1, 0 } }, { { 1, 0, 1 }, { 1, 0 } },
-                                { { 1, 1, 0 }, { 1, 0 } }, { { 1, 1, 1 }, { 1, 0 } },
-                        };
+                                { { 0, 0, 0 }, { 1, 1 } },
+                                { { 0, 0, 1 }, { 0, 1 } },
+                                { { 0, 1, 0 }, { 1, 0 } },
+                                { { 0, 1, 1 }, { 0, 1 } },
+                                { { 1, 0, 0 }, { 1, 0 } },
+                                { { 1, 0, 1 }, { 1, 0 } },
+                                { { 1, 1, 0 }, { 1, 0 } },
+                                { { 1, 1, 1 }, { 1, 0 } }, };
                         if (tipoRede == 1) {
                             redeNeural = new Perceptron(3, 2);
                         } else {
                             redeNeural = new Mlp(3, 2, 2);
                         }
                         treinar(redeNeural, baseRobo, epocas);
-                        // treinar(redeNeural, baseRobo);
                         break;
                     case 5:
                         double[][][] baseSkin = LeitorBase.lerBase("RNA/Skin_NonSkin.txt");
-                        if (baseSkin != null) {
+                        LeitorBase.BaseSeparada baseSeparadaSkin = LeitorBase.separarBase(baseSkin);
+                        if (baseSeparadaSkin != null) {
                             if (tipoRede == 1) {
                                 redeNeural = new Perceptron(3, 1);
+                                treinar (redeNeural, baseSkin, epocas);
                             } else {
-                                redeNeural = new Mlp(3, 5, 1);
+                                System.out.print("\nQuantidade de neurônios na camada intermediária (qtdH): ");
+                                int qtdH = sc.nextInt();
+                                redeNeural = new Mlp(3, qtdH, 1);
+                                treinar(redeNeural, baseSeparadaSkin.baseTreino, baseSeparadaSkin.baseTeste, epocas);
                             }
-                            treinar(redeNeural, baseSkin, epocas);
-                            // treinar(redeNeural, baseSkin);
                         }
                         break;
                     case 0:
@@ -124,13 +129,15 @@ public class RnaMain {
         sc.close();
     }
 
-    // Treina a rede neural por um número fixo de épocas
-    public static void treinar(RNA rna, double[][][] base, int epocas) {
-        System.out.println("\n--- Iniciando Treinamento com " + rna.getClass().getSimpleName() + " ---");
-        for (int e = 0; e < epocas; e++) {
-            double erroAproxEpoca = 0;
-            double erroClassificacaoEpoca = 0;
+    // Treina a rede neural com a base de treino e teste
+    public static void treinar(RNA rna, double[][][] base, double[][][] baseTeste, int epocas) {
+        System.out.println("\n--- Iniciando Treinamento com " + rna.getClass().getName() + " ---");
 
+        for (int e = 0; e < epocas; e++) {
+            double erroAprox = 0.0, erroClassificacao = 0.0;
+            double erroAproxTeste = 0.0, erroClassificacaoTeste = 0.0;
+
+            // Treina com a base de treino
             for (int a = 0; a < base.length; a++) {
                 double[] x = base[a][0];
                 double[] y = base[a][1];
@@ -138,11 +145,11 @@ public class RnaMain {
                 double[] out = rna.treinar(x, y); // Realiza o treinamento de uma amostra, ajustando os pesos
 
                 // Calcula o erro de aproximação da amostra
-                double erroAproxAmostra = 0;
+                double erroAproxAmostra = 0.0;
                 for (int j = 0; j < y.length; j++) {
                     erroAproxAmostra += Math.abs(y[j] - out[j]);
                 }
-                erroAproxEpoca += erroAproxAmostra;
+                erroAprox += erroAproxAmostra;
 
                 // Aplica o limiar de disparo
                 double[] out_t = new double[out.length];
@@ -155,33 +162,62 @@ public class RnaMain {
                 }
 
                 // Verifica se houve erro de classificação na amostra
-                double erroClassificacaoAmostra = 0;
+                double erroClassificacaoAmostra = 0.0;
                 for (int j = 0; j < y.length; j++) {
                     erroClassificacaoAmostra += Math.abs(out_t[j] - y[j]);
                 }
 
-                if (erroClassificacaoAmostra > 0) {
-                    erroClassificacaoEpoca += 1;
+                if (erroClassificacaoAmostra > 0.0) {
+                    erroClassificacao += 1.0;
                 }
             }
 
-            System.out.printf("Época: %d - %f - %.0f\n", e, erroAproxEpoca, erroClassificacaoEpoca);
+            // Testa com a base de teste
+            for (int a = 0; a < baseTeste.length; a++) {
+                double[] x = baseTeste[a][0];
+                double[] y = baseTeste[a][1];
+
+                double[] out = rna.treinar(x, y); // Realiza o treinamento de uma amostra, ajustando os pesos
+
+                // Calcula o erro de aproximação da amostra
+                double erroAproxAmostra = 0.0;
+                for (int j = 0; j < y.length; j++) {
+                    erroAproxAmostra += Math.abs(y[j] - out[j]);
+                }
+                erroAproxTeste += erroAproxAmostra;
+
+                // Aplica o limiar de disparo
+                double[] out_t = new double[out.length];
+                for (int j = 0; j < out.length; j++) {
+                    if (out[j] >= 0.5) {
+                        out_t[j] = 1.0;
+                    } else {
+                        out_t[j] = 0.0;
+                    }
+                }
+
+                // Verifica se houve erro de classificação na amostra
+                double erroClassificacaoAmostra = 0.0;
+                for (int j = 0; j < y.length; j++) {
+                    erroClassificacaoAmostra += Math.abs(out_t[j] - y[j]);
+                }
+
+                if (erroClassificacaoAmostra > 0.0) {
+                    erroClassificacaoTeste += 1.0;
+                }
+            }
+
+            System.out.printf("Época: %d\t| Treino: %f\t- %.0f\t| Teste: %f\t- %.0f\n", e, erroAprox,
+                    erroClassificacao, erroAproxTeste, erroClassificacaoTeste);
         }
     }
+    public static void treinar(RNA rna, double[][][] base, int epocas) {
+        System.out.println("\n--- Iniciando Treinamento com " + rna.getClass().getName() + " ---");
 
-    // Treina a rede neural até atingir erro de classifição zero
-    // ou atingir o número máximo de épocas
-    public static void treinar(RNA rna, double[][][] base) {
-        System.out.println("\n--- Iniciando Treinamento com " + rna.getClass().getSimpleName() + " ---");
-        int e = 1;
-        while (true) {
-            if (e > 100000) {
-                System.out.println("\nNúmero máximo de épocas atingido. Treinamento encerrado.\n");
-                break;
-            }
-            double erroAproxEpoca = 0;
-            double erroClassificacaoEpoca = 0;
+        for (int e = 0; e < epocas; e++) {
+            double erroAprox = 0.0, erroClassificacao = 0.0;
 
+            // Treina com a base de treino
             for (int a = 0; a < base.length; a++) {
                 double[] x = base[a][0];
                 double[] y = base[a][1];
@@ -189,11 +225,11 @@ public class RnaMain {
                 double[] out = rna.treinar(x, y); // Realiza o treinamento de uma amostra, ajustando os pesos
 
                 // Calcula o erro de aproximação da amostra
-                double erroAproxAmostra = 0;
+                double erroAproxAmostra = 0.0;
                 for (int j = 0; j < y.length; j++) {
                     erroAproxAmostra += Math.abs(y[j] - out[j]);
                 }
-                erroAproxEpoca += erroAproxAmostra;
+                erroAprox += erroAproxAmostra;
 
                 // Aplica o limiar de disparo
                 double[] out_t = new double[out.length];
@@ -206,22 +242,18 @@ public class RnaMain {
                 }
 
                 // Verifica se houve erro de classificação na amostra
-                double erroClassificacaoAmostra = 0;
+                double erroClassificacaoAmostra = 0.0;
                 for (int j = 0; j < y.length; j++) {
                     erroClassificacaoAmostra += Math.abs(out_t[j] - y[j]);
                 }
 
-                if (erroClassificacaoAmostra > 0) {
-                    erroClassificacaoEpoca += 1;
+                if (erroClassificacaoAmostra > 0.0) {
+                    erroClassificacao += 1.0;
                 }
             }
 
-            System.out.printf("Época: %d - %f - %.0f\n", e, erroAproxEpoca, erroClassificacaoEpoca);
-            if (erroClassificacaoEpoca == 0) {
-                System.out.println("Treinamento concluído com sucesso!");
-                break;
-            }
-            e++;
+            System.out.printf("Época: %d\t| Treino: %f\t- %.0f\n", e, erroAprox,
+                    erroClassificacao);
         }
     }
 }
